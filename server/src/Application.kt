a75a9c1d357ee410454f3824b5ee3574969b23ce
AlexanderD <shadesave1999@gmail.com>
2020-07-31 06:58:14 +0700

    add db v2 queries

--- server/src/Application.kt
+++ server/src/Application.kt
@@ -1,5 +1,6 @@
 package com.example.server
 
+import com.example.common.CreateCityDto
 import com.example.server.repository.CityRepository
 import io.ktor.application.Application
 import io.ktor.application.call
@@ -7,10 +8,11 @@ import io.ktor.application.install
 import io.ktor.features.ContentNegotiation
 import io.ktor.gson.gson
 import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
+import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.response.respondText
-import io.ktor.routing.get
-import io.ktor.routing.routing
+import io.ktor.routing.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -21,13 +23,26 @@ fun Application.module(testing: Boolean = false) {
         }
     }
 
-    routing {
-        get("/weatherprediction") {
-            call.respond(CityRepository().getAll())
-        }
+    val repository = CityRepository()
 
-        get("/json/gson") {
-            call.respond(mapOf("hello" to "world"))
+    routing {
+        route("/weather/prediction") {
+            get {
+                call.respond(repository.getAll())
+            }
+            post {
+                repository.add(call.receive())
+                call.respond(HttpStatusCode.OK)
+            }
+            delete {
+                val id = call.request.queryParameters["id"]?.toLong()
+                if (id == null) {
+                    call.respond(HttpStatusCode.NotFound)
+                } else {
+                    repository.delete(id)
+                    call.respond(HttpStatusCode.OK)
+                }
+            }
         }
     }
 }
